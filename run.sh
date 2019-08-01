bfa37413566db02ae4ab49fe29a3bc465dfa71d7
hegelpro <palkin.leo@gmail.com>
2019-08-01 15:02:21 +0400

    add sh comand for start project

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker volume create -d vieux/sshfs -o sshcmd=server@192.168.1.52:/home/server/app -o password=sawdqe123 sshvolume
+sudo docker volume create -d vieux/sshfs -o sshcmd=server@192.168.1.52:/home/server/app/server -o password=sawdqe123 sshServer
